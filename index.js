6eed9d922af93e22f6741565cdc8c37cad5502bc
Gage Geigle <gage@gagegeigle.com>
2023-09-14 21:50:21 -0500

    Prepping for deployment to Railway. Removed uneccesary log calls. Modularized API URL components. Removed commented code. Removed goofy API endpoints.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
-//const fetch = require('node-fetch');
 const http = require('http');
 require('dotenv').config();
+
+const LOCAL_URL = '127.0.0.1';
+const LOCAL_PORT = 8080;
+const APP_URL = '127.0.0.1';
+const APP_PORT = 5500;
 
 var wApi = [];
 async function getWeather(weather_url) {
@@ -17,86 +20,26 @@ async function getWeather(weather_url) {
 app.use(express.json());
 
 app.listen(
-    PORT,
-    () => console.log(`it's alive on http://127.0.0.1:${PORT}`)
+    LOCAL_PORT,
+    () => console.log(`Backend alive on ${LOCAL_URL}:${LOCAL_PORT}`)
 );
-
-app.get('/tshirt', (req, res) => {
-
-    res.status(200).send({
-        tshirt: '🎽',
-        size: 'large'
-    })
-});
-
 
 app.get('/wetter', async (req, res) => {
 
-    console.log('got a wetter request');
     wApi = [];
     /* Allow CORS from AngularJS SPA. */
-    res.set('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.set('Access-Control-Allow-Origin', `http://${APP_URL}:${APP_PORT}`);
     var city = req.query.city;
     if(!city){
         res.status(400).send({message: 'Specify a city!'})
     }
     var days = req.query.days;
     if(!days){
-	res.status(400).send({message: 'Specify forecast length!'})
+	    res.status(400).send({message: 'Specify forecast length!'})
     }
-    console.log(`city: ${city}`);
-    console.log(`days: ${days}`);
 
     let url = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHERAPIKEY}&q=${city}&days=${days}&aqi=no`;
-    //let url = 'http://api.weather.gov';
-    //wApi.push('{ "forecast-data": "default data" }');
-
-    console.log(url);
-    //http.get(url, (res) => {
-    //    let body = '';
-
-    //    res.on('data', (chunk) => {
-    //        body += chunk;
-    //    });
-
-    //    res.on('end', () => {
-    //        try {
-    //            let json = JSON.parse(body);
-    //            // Do stuff.
-    //            console.log(json);
-    //        } catch(error) {
-    //            console.error('TRY error: ' + error.message);
-    //        };
-    //    });
-    
-    //}).on('error', (error) => {
-    //    console.error('GET error: ' + error.message);
-    //})
-
-    //let settings = { method: "GET", timeout: "1000" };
-    //fetch(url, settings)
-    //    .then(res => res.json())
-    //    .then((json) => {
-    //        console.log(json);
-    //    });
-
     await getWeather(url);
-    console.log(wApi.length);
-
-    console.log('Done. Sending response.');
 
     res.status(200).send( wApi[0] );
-});
-
-app.post('/tshirt/:id', (req, res) => {
-    const {id} = req.params;
-    const {logo} = req.body;
-
-    if (!logo) {
-        res.status(418).send({message: 'We need a logo!'})
-    }
-
-    res.send({
-        tshirt: `🎽 with your ${logo.logo} and ID of ${id}. Also ${logo.blurb}`,
-    })
 });
